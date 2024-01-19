bc750173cfbb5fd5c078aba749761eca9774cf55
mikeburke <mikeburke.214@gmail.com>
2024-01-19 20:49:16 +0000

    [Task FP.2b] - Started thread running cycleThroughPhases when simulate is called

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -43,7 +43,8 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
+    this->threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
